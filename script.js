a732c2f48aa066b756b058a2afa56e2756640d46
armould <arnaud.rajon@epfl.ch>
2023-06-30 17:07:29 +0200

    buttons to change the grid size and reset the grid

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function createGrid() {
-  const container = document.getElementById("container");
-  for (let i = 0; i < 16; ++i) {
+  for (let i = 0; i < gridSize; ++i) {
     let line = document.createElement("div");
     line.classList = "line";
 
-    for (let j = 0; j < 16; ++j) {
+    for (let j = 0; j < gridSize; ++j) {
       let box = document.createElement("div");
       box.classList = "box";
-      box.style.width = `${96 / 16}vh`;
-      box.style.height = `${96 / 16}vh`;
+      box.style.width = `${96 / gridSize}vh`;
+      box.style.height = `${96 / gridSize}vh`;
       box.addEventListener("mouseover", () => {
         box.style.backgroundColor = getRandRGB();
       });
@@ -34,6 +33,22 @@ function getRandRGB() {
 
 function changeGridSize(grid) {
   const s = prompt("Enter the new grid size");
+  if (s > 0) {
+    gridSize = s;
+  }
+  //stylesheet.insertRule(`.box { width: ${96 / gridSize}vh;}`, 0);
+  container.innerHTML = "";
+  createGrid();
+}
+function resetGrid() {
+  const boxes = Array.from(document.getElementsByClassName("box"));
+  for (let i = 0; i < boxes.length; ++i) {
+    boxes[i].style.backgroundColor = "white";
+  }
 }
 
+let gridSize = 16;
+const container = document.getElementById("container");
+document.getElementById("gsize").addEventListener("click", changeGridSize);
+document.getElementById("reset").addEventListener("click", resetGrid);
 createGrid();
